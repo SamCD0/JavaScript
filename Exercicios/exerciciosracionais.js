0248316023d0082fb71ae26fae9824d84b5953c6
SamCD0 <frankerboylol@gmail.com>
2025-01-29 09:55:40 -0300

    exercicios

---
+++ Exercicios/exerciciosracionais.js
@@ -0,0 +1,40 @@
+let num1 = 6
+let num2 = 9
+let res = num1 > num2
+let res2 = num1 <= num2
+let res3 = num1 == num2
+console.log(res)
+console.log(res2)
+console.log(res3)
+console.log("--------------")
+let idade = 21
+let votar = idade > 16
+let forcado = 18 >= idade <= 70
+console.log(votar)
+console.log(forcado)
+console.log("--------------")
+let pessoaidade = 39
+let adulto = pessoaidade >= 18
+let idoso = pessoaidade >= 60
+console.log(adulto)
+console.log(idoso)
+console.log("--------------")
+let usuario = "admin"
+let nomedeusuario = usuario == "admin"
+console.log(nomedeusuario)
+console.log("--------------")
+let numero = 6
+let div3 = numero % 3 == 0 
+let div5 = numero % 5 == 0
+console.log(div3)
+console.log(div5)
+console.log("--------------")
+let saldo = 500
+let saque = 550
+let suficiente = saldo >= saque
+let suficientextra = saldo >= saque
+console.log(suficiente)
+console.log(suficientextra)
+
+
+
