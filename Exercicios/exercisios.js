f7133f439f9d938c110ea8e2c2dcb544c9ac5d71
SamCD0 <frankerboylol@gmail.com>
2025-01-29 08:19:04 -0300

    Initial commit

---
+++ Exercicios/exercisios.js
@@ -0,0 +1,41 @@
+//exercício 1
+let a = 4
+let b = 2
+
+let soma = a + b
+let sub = a - b
+let mult = a * b
+let div = a / b
+let resto = a % b
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(resto)
+//exercício 2
+let largura = 5
+let altura = 3
+
+let area = largura * altura
+console.log("area =", area)
+//exercício 3
+let num = 7
+let metade = num/2
+let dobro = num * 2
+console.log(metade)
+console.log(dobro)
+//exercício 4
+let nota1 = 9
+let nota2 = 6
+let nota3 = 7
+let notaSoma = nota1 + nota2 + nota3
+let notafinal = notaSoma/3
+console.log("a média das notas é", notafinal)
+//exercício 5
+let tempC = 30
+let tempF = tempC * 1.8 + 32
+console.log("a temperatura em Fahrenheit é", tempF)
+
+
+
---
+++ Operadores/aritimeticos.js
@@ -0,0 +1,29 @@
+//console.log("Olá, Mundo!")
+
+//declarar variavel
+
+let nome = "Juliana"
+var nomes = "Juliana"
+
+//ctrl + ; -> COMENTARIOS
+console.log(nome)
+
+let idade = 24
+console.log("Minha idade é ", idade, "anos")
+//contenação = juntar texto e valor
+
+//operações matematicas
+let a = 3
+let b = 2
+
+let soma = a + b
+let sub = a - b
+let mult = a * b
+let div = a / b
+let resto = a % b
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(resto)
