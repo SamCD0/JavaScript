5c6b2978ff609a5ff6e5e8403547e6cafcaea3e7
SamCD0 <frankerboylol@gmail.com>
2025-05-13 10:24:51 -0300

    de nv
    jhrwghrwhw4h4wh

--- Quiz Com Tema/script.js
+++ Quiz Com Tema/script.js
@@ -1,21 +1,57 @@
 const perguntas = [
     {
-        pergunta: "Qual o nome da nossa escola?",
-        opcoes: ["Luis Eulalio de Bueno Vidigal Filho", "luis Eulalio", "Luis de BUeno", "Luis Eulalio de Vidigal"],
+        pergunta: "Qual é a raiz quadrada de 64",
+        opcoes: ["6", "8", "4", "32"],
+        correta: 1
+    },
+    {
+        pergunta: "Quanto é 20% de 25?",
+        opcoes: ["5", "4", "10", "3"],
         correta: 0
     },
     {
-        pergunta: "Qual o nome do nosso curso?",
-        opcoes: ["Analise e Desenvolvimento de Sistemas", "Sistemas de informação", "Desenvolvimento de sistemas", "Ciência da Computação"],
+        pergunta: "150 X 40",
+        opcoes: ["4500", "6200","6500", "6000"],
+        correta: 3
+    },
+    {
+        pergunta: "1 + 1 - 1 X 0",
+        opcoes: ["1", "2", "3", "0"],
+        correta: 1
+    },
+    {
+        pergunta: "A + 5 = 12;A = ",
+        opcoes: ["7", "8", "6", "5"],
+        correta: 0
+    },
+    {
+        pergunta: "Quanto vale PI",
+        opcoes: ["2", "3", "3,14", "3,27"],
         correta: 2
     },
     {
-        pergunta: "Em que ano foi fundada a nossa escola?",
-        opcoes: ["1999", "1942","1985", "1970"],
+        pergunta: "Quantos segundos tem em um mês",
+        opcoes: ["2.592,000", "3.436,080,000", "98,654", "512,053"],
+        correta: 0
+    },
+    {
+        pergunta: "Quem inventou o teorema de pitágoras",
+        opcoes: ["Eu", "Pitágoras", "Pele", "babilônios "],
+        correta: 3
+    },
+    {
+        pergunta: "3 + 3!",
+        opcoes: ["9", "6", "3", "1 "],
+        correta: 0
+    },
+    {
+        pergunta: "10 + 9",
+        opcoes: ["21", "19", "17", "190"],
         correta: 1
+    },
 
-    }
-] 
+    
+]
 let perguntaAtual = 0
 let pontuacao = 0
 let erros = 0
