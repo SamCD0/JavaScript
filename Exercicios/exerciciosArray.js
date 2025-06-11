2137a51bb8b3390c02aa6382f6c3f06d25462784
SamCD0 <frankerboylol@gmail.com>
2025-06-11 11:28:28 -0300

    num
    a

--- Exercicios/exerciciosArray.js
+++ Exercicios/exerciciosArray.js
@@ -1,8 +1,68 @@
-let filmes = ["The courage", "todo mundo em panico 4", "Nós"]
+cor = ["Vermelho", "Preto", "Roxo"]
+for (let c = 0; c < cor.push(); c++) {
+    console.log(cor[c]);
+}
+console.log("Adicionando no final")
+cor.push("Rosa")
+console.log(cor)
+console.log("Adicionando no final");
+cor.push("Rosa");
+console.log(cor);
+////////////Exercicio 3
+let cores = ["Vermelho", "Azul", "Verde"];
+cores.push("Amarelo");
+console.log("Array atualizado:");
+for (let i = 0; i < cores.length; i++) {
+    console.log(cores[i]);
+}
 
-console.log(filmes[0])
+////////////Exercicio 4
+console.log("---------------------------------------------------------")
+let numero = [5 , 7, 8, 6]
+numero.pop
+for (let n = 0; n < numero.pop(); n++) {
+    console.log(numero [n]);
+let numeros = [10, 20, 30, 40];
+numeros.pop();
+console.log("Array atualizado:");
+}
+for (let i = 0; i < numero.length; i++) {
+    console.log(numero[i]);
+}
 
-let frutas = ["maça", "banana", "uva", "pera", "abacate"]
-console.log(filmes[2])
+////////////Exercicio 5
+console.log("---------------------------------------------------------")
+let cidades = ["São Paulo", "Rio de Janeiro"];
+cidades.unshift("Curitiba");
+console.log("Array atualizado:");
+for (let i = 0; i < cidades.length; i++) {
+    console.log(cidades[i]);
+}
+console.log("Removido do final")
+cor.pop()
+console.log(numero)
 
-let cores = ["Amarelo", "Branco", "Preto"]
+////////////Exercicio 6
+console.log("---------------------------------------------------------")
+let animais = ["Cachorro", "Gato", "Pássaro"];
+animais.shift();
+console.log("Array atualizado:");
+for (let i = 0; i < animais.length; i++) {
+    console.log(animais[i]);
+}
+////////////Exercicio 7
+console.log("---------------------------------------------------------")
+let carros = ["Ferrari", "BMW", "Toyota", "Honda"];
+console.log("Quantidade de carros:", carros.length);
+
+////////////Exercicio 8
+console.log("---------------------------------------------------------")
+let numeros1 = [];
+numeros1.push(5);
+numeros1.push(10);
+numeros1.push(15);
+numeros1.unshift(20);
+numeros1.pop();
+numeros1.shift();
+console.log("Array final:", numeros1);
+console.log("Tamanho do array:", numeros1.length);
